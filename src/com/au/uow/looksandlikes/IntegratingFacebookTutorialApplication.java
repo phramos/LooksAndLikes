5e9151ccb46bac4cde5078e5941367bcdc034fd6
Alessandro <aaf665@uowmail.edu.au>
2014-10-27 23:15:37 +1100

    First login done using Parse and Facebook

---
+++ src/com/au/uow/looksandlikes/IntegratingFacebookTutorialApplication.java
@@ -0,0 +1,24 @@
+package com.au.uow.looksandlikes;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseFacebookUtils;
+
+public class IntegratingFacebookTutorialApplication extends Application {
+
+	static final String TAG = "MyApp";
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		Parse.initialize(this, "LNRiRaziB3sqAU9Txgy8FvLyEOYq4TcGlB1t3DjT",
+				"cKhsPSV1sBPsLXUUfrbEE7NFh0Pa7dojQx3HhLsK");
+
+		// Set your Facebook App Id in strings.xml
+		ParseFacebookUtils.initialize(getString(R.string.app_id));
+
+	}
+
+}
